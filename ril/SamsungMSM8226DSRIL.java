61eab1839e3e0fdcac76c802389dff77708e3863
Mukul Soni <mukulsoni2@gmail.com>
2015-09-12 13:10:50 +0700

    Reverted some RIL commits to get back call audio

--- ril/SamsungMSM8226DSRIL.java
+++ ril/SamsungMSM8226DSRIL.java
@@ -297,7 +297,7 @@ public class SamsungMSM8226DSRIL extends RIL implements CommandsInterface {
         int lteCqi = p.readInt();
         int tdScdmaRscp = p.readInt();
         // constructor sets default true, makeSignalStrengthFromRilParcel does not set it
-        boolean isGsm = false;
+        boolean isGsm = true;
 
         if ((lteSignalStrength & 0xff) == 255 || lteSignalStrength == 99) {
             lteSignalStrength = 99;
